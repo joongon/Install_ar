e083ced14ad2e1d8aff7539aab35c1be5a5c2578
RigidBody <42177180+joongon@users.noreply.github.com>
2021-09-01 18:08:45 +0900

    Update install_ar.sh

--- install_ar.sh
+++ install_ar.sh
@@ -18,13 +18,14 @@ else
     curl https://rclone.org/install.sh | sudo bash
 fi
 
-sleep 2
+#sleep 2
 
 # 설치용 폴더 생성
-cd /root
+#cd /root
 
-[ -d install_ar ] || mkdir /root/install_ar
-
+#[ -d /root/install_ar ] || mkdir /root/install_ar
+mkdir /root/install_ar
+cd /root/install_ar
 # pip3 설치 진행
 
 if which pip3 > /dev/null; then
@@ -34,7 +35,7 @@ else
     mv get-pip.py /root/install_ar
     python3 /root/install_ar/get-pip.py
 
-        if which pip3 > /dev/nill; then
+        if which pip3 > /dev/null; then
             echo "pip3 has been successfully installed on your NAS."
         else
             echo "somthing is wrong. check it out. try again later."
@@ -44,8 +45,9 @@ fi
 curl -O https://cdn.rigidsolution.com/file/autorclone.zip
 
 sleep 5
-source /root/.bashrc
-cd /root/install_ar
+
+#volume1, volume2, volume3 가 있다고 가정, volume3, volume2, volume1 우선순위로 설치 진행
+#해당 설치 위치에 따라 ar.sh내의 설치 root 이동명령에 반영
 
 is_v2=$(df -h | grep volume2)
 is_v3=$(df -h | grep volume3)
@@ -57,16 +59,18 @@ if [ $install_path = "default" ]; then
     7z x autorclone.zip -o/volume1/homes
     echo "alias ar=\"bash /volume1/homes/AutoRclone/ar.sh\"" >> /root/.bashrc
     sudo pip3 install -r /volume1/homes/AutoRclone/requirements.txt
-    
+        
 elif [ $install_path = "p_v2" ]; then
     7z x autorclone.zip -o/volume2/
     echo "alias ar=\"bash /volume2/AutoRclone/ar.sh\"" >> /root/.bashrc
     sudo pip3 install -r /volume2/AutoRclone/requirements.txt
+    sed -i 's/cd \/volume1\/homes\/AutoRclone/cd \/volume2\/AutoRclone/g' /volume2/AutoRclone/ar.sh
 
 elif [ $install_path = "p_v3" ]; then
     7z x autorclone.zip -o/volume3/
     echo "alias ar=\"bash /volume3/AutoRclone/ar.sh\"" >> /root/.bashrc
     sudo pip3 install -r /volume3/AutoRclone/requirements.txt
+    sed -i 's/cd \/volume1\/homes\/AutoRclone/cd \/volume3\/AutoRclone/g' /volume3/AutoRclone/ar.sh
 else
     prinf "It is not possible to find right path to install AutoRclone on your System,\n
            so check out the path and try again.\n"
@@ -76,4 +80,8 @@ fi
 rm -rf /root/install_ar
 
 printf "\nAll installation procedures have been successfully completed.\n\n"
-printf "For using AutoRclone you need to copy Service Account files into $INSTALL_ROOT/accounts."
+printf "For using AutoRclone you need to copy Service Account files into $INSTALL_ROOT/accounts.\n\n"
+
+source /root/.bashrc
+sleep 5
+source /root/.bashrc
